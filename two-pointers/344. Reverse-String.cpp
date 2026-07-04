ecef21eac18ec942cae1b3d9c5b42c436a638720
stutimahesh <stuti05mahesh@gmail.com>
2026-07-04 22:35:01 +0530

    Add Problem-344

---
+++ two-pointers/344. Reverse-String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i=0;
+        int j= s.size()-1;
+        while(i<j){
+            char a= s[i];
+            s[i]= s[j];
+            s[j]= a;
+            i++;j--;
+        }
+    }
+};
