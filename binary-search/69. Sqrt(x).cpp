55e3422f4254370824a09b68dc24bfbc3fb7af35
stutimahesh <stuti05mahesh@gmail.com>
2026-07-09 23:15:51 +0530

    Add Problem-69

---
+++ binary-search/69. Sqrt(x).cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1)
+            return x;
+
+        int low = 1, high = x;
+        int ans = 0;
+
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+            long long square = mid * mid;
+
+            if (square == x)
+                return mid;
+            else if (square < x) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
