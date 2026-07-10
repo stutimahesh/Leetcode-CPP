0ef87bfcf9e7109301f9e8da6ba67c5ab8ec60bb
stutimahesh <stuti05mahesh@gmail.com>
2026-07-10 22:01:39 +0530

    Add Problem-704

---
+++ binary-search/704. Binary-Search.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0,high=nums.size()-1;
+        int ans=-1;
+        while (low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) { ans=mid;break; }
+            else if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return ans;
+    }
+};
