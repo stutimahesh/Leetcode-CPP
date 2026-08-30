152f9fd78d78448774fcc30a17fb0ca5f1f1c385
stutimahesh <stuti05mahesh@gmail.com>
2026-08-30 21:49:39 +0530

    Add Problem-3024

---
+++ math/3024. Type-of-Triangle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        int a=nums[0],b=nums[1],c=nums[2];
+
+        if(a+b<=c || b+c<=a || c+a<=b) return "none";
+
+        if(a==b && b==c) return "equilateral";
+        else if(a==b || a==c || b==c) return "isosceles";
+        else return "scalene";        
+    }
+};
